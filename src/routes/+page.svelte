307d53a5de98b0ec4d3f1f3124f5836048475c25
Saryass <saryass.aree@gmail.com>
2025-01-05 15:41:58 +0300

    testing first commet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="bg-yellow-400">Welcome to SvelteKit</h1>
+<p class="bg-red-300"> Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
